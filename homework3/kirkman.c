4b42a3c83e8ae59e2be9b6dfe85e2b8fd01556d4
Dustin Kane <dustinpkane@gmail.com>
2016-04-02 19:44:00 -0700

    it works! Probably should remove some of the function calls to speed it up?

--- homework3/kirkman.c
+++ homework3/kirkman.c
@@ -6,12 +6,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
+
 
 
 typedef struct {
 	int layout[NUM_DAYS][NUM_ROWS][GIRLS_IN_ROW]; // How the girls are laid out for each day.
 	bool partners[NUM_GIRLS][NUM_GIRLS];	// Array of arrays, each representing girls each girl has stood with
-	bool success; // True if we successfully placed the girl, false otherwise
 } GirlLayout;
 
 GirlLayout copyLayout(GirlLayout layout);
@@ -19,91 +20,156 @@ void printLayout(GirlLayout layout);
 
 int funcCalls;
 
-
-GirlLayout putGirl(GirlLayout layout, int girl, int day, int row) {
-	funcCalls++;
-	//printf("Trying girl %d on day %d in row %d\n", girl, day, row);
-	bool found = false;
-	for (int i = 0; i < GIRLS_IN_ROW; ++i) {
-		int thisGirl = layout.layout[day][row][i];
-		// That's a spot!
-		if (thisGirl == -1) {
-			found = true;
-			break;
-		}
-		// If we've stood next to her before, we can't be here
-		if (layout.partners[girl][thisGirl]) {
-			//printf("dis bitch %d is already here\n", thisGirl);
-			found = false;
-			break;
-		}
-	}
-	if (found) {
-		//printf("Found a spot!\n");
-		GirlLayout newLayout = copyLayout(layout);
+/*
+	day:  day we're trying to fill up
+	row: row we're trying to put her in
+	pos: position in row we're trying to put her in
+	girlsPlacedToday: boolean array for each girl. We have to look up the girl we're
+					  trying to place to see if she's already been placed today
+*/
+bool putSpot(GirlLayout * layout, int day, int row, int pos, bool * girlsPlacedToday) {
+	
+	// If it's the first slot of the day, we have to reset the girlsPlacedToday
+	if (row == 0 && pos == 0) {
+		girlsPlacedToday = malloc(NUM_GIRLS * sizeof(bool));
+		memset(girlsPlacedToday, false, NUM_GIRLS * sizeof(bool));
+		// However, the first GIRLS_IN_ROW girls are in place already
 		for (int i = 0; i < GIRLS_IN_ROW; ++i) {
-			int thisGirl = layout.layout[day][row][i];
-			// That's the spot!
-			if (thisGirl == -1) {
-				// Stick me in there
-				newLayout.layout[day][row][i] = girl;
-				break;
-			}
-			// Remember we stood next to this one from now on
-			else{
-				newLayout.partners[girl][thisGirl] = true;
-			}
+			girlsPlacedToday[i] = true;
 		}
-		
-		// If this was the last girl, on to the next day (maybe)
-		if (girl >= NUM_GIRLS - 1) {
-			// If it's the last day, we won! The nightmare is over
-			if (day >= NUM_DAYS - 1) {
-				//printf("The nightmare should be over\n");
-				newLayout.success = true;
-				return newLayout;
-			}
-			// Otherwise, start with girl 0 tomorrow
-			else {
-				//printf("Onto the next day!\n");
-				return putGirl(newLayout, 0, day + 1, 0);
-			}
-		}
-		// Otherwise, try this next girl from the top today
-		//printf("Trying next grill\n");
-		GirlLayout result = putGirl(newLayout, girl + 1, day, 0);
-		if (result.success) {
-			//printf("I am girl %d on day %d and being in row %d worked\n", girl, day, row);
-			return result;
-		}
-		// She didn't work? I guess we have to try me on the next row now
 	}
 
-	//printf("I am girl %d on day %d and row %d did not work for me\n", girl, day, row);
-	// Couldn't put me in this row, or putting me in that row leads to an unfortunate series of events?
-	// Welp, try to stick me in the next row.
-	// Unless that was the last row...
-	if (row >= NUM_ROWS - 1) {
-		//printf("Last row scamaz :(\n");
-		// in which case we're screwed
-		layout.success = false;
-		return layout;
+	// We have already placed the first GIRLS_IN_ROW girls for each day
+	int firstGirl = GIRLS_IN_ROW;
+	// We've done the first girl for the first GIRLS_IN_ROW rows already
+	if (row < GIRLS_IN_ROW) {
+		// First position is done
+		if (pos == 0) {
+			return putSpot(layout, day, row, pos + 1, girlsPlacedToday);
+		}
 	}
-	// pass in the old layout, this one is trash
-	//printf("Trying the next row\n");
-	return putGirl(layout, girl, day, row + 1);
+	else {
+		// Since first column increases downward, our minimum is whoever is above us + 1
+		if (pos == 0) {
+			int topGirl = layout->layout[day][row - 1][pos] + 1;
+			if (topGirl > firstGirl) {
+				firstGirl = topGirl;
+			}
+		}
+	}
+	// Stick this here so it doesn't count the skips
+	funcCalls++;
+
+	// If we're not the first in this row
+	if (pos > 0) {
+		// Rows are sequential, so we can elimintate girls less than the girl to our left
+		int leftGirl = layout->layout[day][row][pos - 1] + 1;
+		if (leftGirl > firstGirl) {
+			firstGirl = leftGirl;
+		}
+	}
+	// If we are in the middle position in the top row
+	if (row == 0 && pos == GIRLS_IN_ROW / 2) {
+		// Middle girls increase as well
+		int middleGirl = layout->layout[day - 1][row][pos] + 1;
+		if (middleGirl > firstGirl) {
+			firstGirl = middleGirl;
+		}
+	}
+
+	int lastGirl = NUM_GIRLS - 1;
+	// last GIRLS_IN_ROW girls can only go in the last column. So if we're not in the last column, the last
+	// girl we can place is NUM_GIRLS - 1 - GIRLS_IN_ROW
+	if (pos < GIRLS_IN_ROW - 1) {
+		lastGirl = NUM_GIRLS - 1 - GIRLS_IN_ROW;
+	}
+
+	for (int girl = firstGirl; girl <= lastGirl; ++girl) {
+		//if can go in this slot
+
+		// Have we been placed today?
+		if (!girlsPlacedToday[girl]) {
+			// check the other girls in the row to see if we've been with them already
+			bool problem = false;
+			for (int i = 0; i < pos; ++i) {
+				int thisGirl = layout->layout[day][row][i];
+				if (layout->partners[girl][thisGirl]) {
+					problem = true;
+				}
+			}
+			if (!problem) {
+				// So far, the story checks out and we can stick her in
+				layout->layout[day][row][pos] = girl;
+				// Update our table of stood-next-to's
+				for (int i = 0; i < pos; ++i) {
+					int thisGirl = layout->layout[day][row][i];
+					layout->partners[girl][thisGirl] = true;
+					layout->partners[thisGirl][girl] = true;
+				}
+				// Mark that we placed this girl today
+				girlsPlacedToday[girl] = true;
+
+				// Move on to the next one
+				int newDay = day;
+				int newRow = row;
+				int newPos = pos + 1;
+				if (newPos == GIRLS_IN_ROW) {
+					newPos = 0;
+					newRow += 1;
+				}
+				if (newRow == NUM_ROWS) {
+					newRow = 0;
+					newDay += 1;
+				}
+				if (newDay == NUM_DAYS) {
+					// we won!
+					return true;
+				}
+				bool result = putSpot(layout, newDay, newRow, newPos, girlsPlacedToday);
+				if (result) {
+					// We also win, because we successfully placed this girl in this row
+					// Was this a new day? If so we need to free the girlsPlacedToday
+					if (newDay > day) {
+						free(girlsPlacedToday);
+					}
+					return true;
+				}
+				// If it wasn't a success we have to backtrack and undo setting the girl
+
+				layout->layout[day][row][pos] = -1;
+				for (int i = 0; i < pos; ++i) {
+					int thisGirl = layout->layout[day][row][i];
+					layout->partners[girl][thisGirl] = false;
+					layout->partners[thisGirl][girl] = false;
+				}
+				girlsPlacedToday[girl] = false;
+			}
+		}
+	}
+	return false;
 }
 
-GirlLayout copyLayout(GirlLayout layout) {
-	GirlLayout newLayout;
+bool setup(GirlLayout * layout) {
+	// We can do the first column, so we can get that out of the way.
+	for (int row = 0; row < NUM_ROWS; ++row) {
+		for (int girl = 0; girl < GIRLS_IN_ROW; ++girl) {
+			// Just do it in order
+			layout->layout[0][row][girl] = row * GIRLS_IN_ROW + girl;
 
-	int layoutLength = NUM_DAYS * NUM_ROWS * GIRLS_IN_ROW;
-	memcpy(&newLayout.layout, &layout.layout, layoutLength * sizeof(int));
-	int partnerLength = NUM_GIRLS * NUM_GIRLS;
-	memcpy(&newLayout.partners, &layout.partners, partnerLength * sizeof(bool));
+			// Set the partner table. Doesn't matter if someone marks themself as true
+			for (int neighbor = 0; neighbor < GIRLS_IN_ROW; ++neighbor) {
+				layout->partners[girl][neighbor] = true;
+			}
+		}
+	}
+	// The first girl for every subsequent day, going down, is always 0, 1, 2
+	for (int day = 1; day < NUM_DAYS; ++day) {
+		for (int girl = 0; girl < GIRLS_IN_ROW; ++girl) {
+			layout->layout[day][girl][0] = girl;
+		}
+	}
 
-	newLayout.success = layout.success;
-	return newLayout;
+	return true;
 }
 
 void printLayout(GirlLayout layout) {
@@ -111,13 +177,9 @@ void printLayout(GirlLayout layout) {
 		for (int day = 0; day < NUM_DAYS; ++day) {
 			for (int girl = 0; girl < GIRLS_IN_ROW; ++girl) {
 				int thisGirl = layout.layout[day][row][girl];
-				
-				// If only one digit, add an extra space
-				if (thisGirl / 10 == 0) {
-					printf(" ");
+				if (thisGirl > -1) {
+					printf("%c", thisGirl + 'A');
 				}
-				
-				printf("%d ", thisGirl);
 			}
 			printf("\t");
 		}
@@ -132,13 +194,17 @@ int main(){
 	memset(&grills.layout, -1, NUM_DAYS * NUM_ROWS * GIRLS_IN_ROW * sizeof(int));
 	memset(&grills.partners, 0, NUM_GIRLS * NUM_GIRLS * sizeof(bool));
 
-	grills.success = 0;
+	bool * girlsPlacedToday = malloc(NUM_GIRLS * sizeof(bool));
+	bool result = setup(&grills);
+	if (result) {
+		result = putSpot(&grills, 1, 0, 0, girlsPlacedToday);
+	}
 
-	GirlLayout result = putGirl(grills, 0, 0, 0);
+	free(girlsPlacedToday);
 
-	printf("Success: %d FuncCalls: %d\n", result.success, funcCalls);
+	printf("Success: %d FuncCalls: %d\n", result, funcCalls);
 
-	printLayout(result);
+	printLayout(grills);
 
 	return 0;
 } 
