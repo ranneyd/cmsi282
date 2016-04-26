3a787a9b10d9fa8d98489734585c0adba68acb99
Dustin Kane <dustinpkane@gmail.com>
2016-04-26 11:03:26 -0700

    NAXX OUT

--- homework3/BacktrackingEngine.js
+++ homework3/BacktrackingEngine.js
@@ -2,12 +2,27 @@
 // getNext: function that takes some kind of value as a parameter. The idea is, we will try a value,
 // and when we find out it doesn't work, we need the next one. getNext takes the previous value and
 // gives us what we should try next. If nothing is passed in, it should give us the first value we
-// should try. It should return undefined if there is no next value.
+// should try. It should return false if there is no next value.
 
 // checkResults takes a results array and returns false if it fails, "partial" if it is a partial
 // solution, and true if it is a complete solution.
 module.exports = (getNext, checkResult) => {
     let generator = (arr) => {
+        let next;
+        while(next = getNext(next)) {
+            arr.push(next);
+            let result = checkResult(arr);
+            if(result === "partial") {
 
+            }
+            else if (result) {
+
+            }
+            else {
+                // Take off the one we pushed
+                arr.pop();
+                return false;
+            }
+        }
     };
 }
